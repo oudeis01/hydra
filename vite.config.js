cc6f37bc883271c6e3fc7073e550f325e0c6bdbf
choiharam <altitudinem@gmail.com>
2025-11-11 21:55:55 +0100

    staging

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,36 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    //define: { global: {} },
-    base: '',
-    define: {
-        'process.env': {},
-        // 'global.window': 'window'
-        // global: {}
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: 'globalThis'
-            }
-        }
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  base: '',
+  define: {
+    'process.env': {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
     }
-})
+  },
+
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: '0.0.0.0',
+  },
+
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+
+  build: {
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+}))
